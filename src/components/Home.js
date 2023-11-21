b622b1f7de7be9c8ac724138320823dc8af9b078
harshNileshDesai <harsh.desai@kasisto.com>
2023-11-21 14:07:39 +0530

    4th commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,12 +28,32 @@ const Home = () => {
       });
       setLoading(false);
       if (!res.ok) {
-        alert(`${rfid} - NOT ALLOWED`);
-        return;
+        // alert(`${rfid} - NOT ALLOWED`);
+        setData({
+          name: null,
+          rfid: rfid,
+          event: null,
+          status: false
+        })
+        console.log(rfid)
+        // return;
       }
-      const data = await res.json();
-      console.log(data)
-      setData(data);
+      else {
+        const data = await res.json();
+        console.log(data)
+        setData(data);
+      }
+      const form = document.getElementById('form');
+      const heading = document.getElementById('heading');
+      const display = document.getElementById('display');
+      setTimeout(() => {
+        heading.classList.toggle('hidden');
+        form.classList.toggle('hidden');
+        display.classList.toggle('hidden');
+      }, 2000);
+      heading.classList.toggle('hidden');
+      form.classList.toggle('hidden');
+      display.classList.toggle('hidden');
     }
 
   }
@@ -44,9 +64,9 @@ const Home = () => {
       <div className='h-[85vh] w-full flex justify-center items-center '>
         <div className=" w-3/4 h-3/4  flex flex-col justify-center transition-all ">
           {/* Heading */}
-          <h1 className='my-3 font-medium text-xl uppercase '>Enter the RFID to check for student's status</h1>
+          <h1 className='my-3 font-medium text-xl uppercase ' id='heading'>Enter the RFID to check for student's status</h1>
           {/* Form for search */}
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} id='form'>
             <div className="my-3 rfid">
               <label htmlFor="rfid"></label>
               <input type="text" name="rfid" id="rfid" className='border border-slate-500 px-4 py-2 w-full rounded-md focus-within:outline-blue-300 focus:border-blue-300 ' />
@@ -56,28 +76,23 @@ const Home = () => {
             </div>
           </form>
           {/* Display the data */}
+          <div id='display' className='hidden'>
           {
             data !== null ?
-              <table className='w-full my-5'>
-                <thead >
-                  <tr className='border-2 border-black'>
-                    <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>Name</td>
-                    <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>RFID</td>
-                    <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>Event</td>
-                    {/* <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>Status</td> */}
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className='border-2 border-black'>
-                    <td className='w-1/3 py-2 border-r-2 border-black text-center'>{data?.name}</td>
-                    <td className='w-1/3 py-2 border-r-2 border-black text-center'>{data?.rfid}</td>
-                    <td className='w-1/3 py-2 border-r-2 border-black text-center'>{data?.event}</td>
-                    {/* <td className='py-2 border-r-2 border-black text-center'>{data?.status===true? "ALLOWED": "NOT ALLOWED"}</td> */}
-                  </tr>
-                </tbody>
-              </table>
+              <div className=' flex flex-col my-7 '>
+                <div className=' font-medium  inline'>
+                  <span className='block my-1 text-xl'>RFID: {data?.event===null?data?.rfid:''}</span>
+                  <span className='block my-1 text-xl'>{data?.event!==null?data?.event:''}</span>
+                  <span className='block my-1 text-xl'>{data?.name!==null?data?.name: ''}</span>
+                  <div className='my-4' >
+                  <span className={`px-4 py-2 rounded-full ${data?.status===true?'bg-green-500':'bg-red-500'} text-white`}>{data?.status === true ? "ALLOWED" : "NOT ALLOWED"}</span>
+                  </div>
+                </div>
+              </div>
               : ''
           }
+
+          </div>
         </div>
       </div>
     </>
@@ -85,3 +100,25 @@ const Home = () => {
 }
 
 export default Home
+
+
+/*
+<table className='w-full my-5'>
+                <thead >
+                  <tr className='border-2 border-black'>
+                    <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>Name</td>
+                    <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>RFID</td>
+                    <td className='w-1/3 py-2 border-r-2 border-black text-center font-medium bg-slate-100'>Event</td>
+                    
+                    </tr>
+                    </thead>
+                    <tbody>
+                      <tr className='border-2 border-black'>
+                        <td className='w-1/3 py-2 border-r-2 border-black text-center'>{data?.name}</td>
+                        <td className='w-1/3 py-2 border-r-2 border-black text-center'>{data?.rfid}</td>
+                        <td className='w-1/3 py-2 border-r-2 border-black text-center'>{data?.event}</td>
+                        
+                      </tr>
+                    </tbody>
+                  </table>
+*/
